e04604d8410810e2948a565cd6e48ca694e1ae0e
Officialbube <ebubenkereebube@gmail.com>
2025-03-08 01:31:54 +0100

    fixed play button position

--- src/app/[type]/[id]/page.tsx
+++ src/app/[type]/[id]/page.tsx
@@ -117,7 +117,7 @@ const page = async ({ params }: { params: { id: string; type: string } }) => {
           </div>
           <Options />
         </div>*/}
-        <div className="flex justify-center h-[500px] z-20 mr-5 lg:mt-20">
+        <div className="flex justify-center items-center h-[500px] z-20 mr-5 lg:mt-20">
           <PlayButton
             getSeasonList={getSeasonList}
             imdbId={data?.externalIds?.imdb_id}
